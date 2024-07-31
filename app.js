8481468922622e6325a48e2201caf247884a8052
Gaston Lopez <gastonlopez436@gmail.com>
2024-07-30 22:19:42 -0300

    continue dev site

---
+++ app.js
@@ -0,0 +1,11 @@
+/* const textarea = document.querySelector(".output_text_area textarea");
+const textOverlay = document.querySelector(".text-overlay");
+
+textarea.addEventListener("input", () => {
+    if (textarea.value.trim() !== "") {
+        textOverlay.style.display = "none";
+    } else {
+        textOverlay.style.display = "block";
+    }
+});
+ */
